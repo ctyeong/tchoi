87076827e84df1084542b891924dc0a6e831caf5
Taeyeong Choi <ctyeong@gmail.com>
2020-12-02 20:45:14 +0000

    Change theme

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Testing",
+        "excerpt":"This is my test post.  ","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "http://localhost:4000/tchoi/jekyll/update/2020/12/01/welcome-to-jekyll-copy.html",
+        "teaser": null
+      },{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "http://localhost:4000/tchoi/jekyll/update/2020/12/02/welcome-to-jekyll.html",
+        "teaser": null
+      }]
